adeea8e4eb9070962d3b5b32d762dc3b42998ab4
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-08-10 23:08:50 +0530

    Add files via upload

---
+++ Array_mathematics.py
@@ -0,0 +1,10 @@
+import numpy as np
+n, m = map(int, input().split())
+a = np.array([list(map(int, input().split())) for _ in range(n)])#takin array input
+b = np.array([list(map(int, input().split())) for _ in range(n)])
+print(np.add(a, b))        # Addition
+print(np.subtract(a, b))   # Subtraction
+print(np.multiply(a, b))   # Multiplication
+print(np.floor_divide(a, b)) # Integer Division (Floor Division)
+print(np.mod(a, b))        # Modulus
+print(np.power(a, b))      # Power
