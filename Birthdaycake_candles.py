16fd93abb5666513522e2b9fff66e3e37aed68b1
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-09-04 21:07:18 +0530

    Add files via upload

---
+++ Birthdaycake_candles.py
@@ -0,0 +1,36 @@
+#https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+#code:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    tallest = max(candles)
+    count = candles.count(tallest)
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
