c5fef4e41efcaa5e13460119ba6ffcf5cb003f50
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-10-29 18:02:47 +0530

    Add files via upload

---
+++ Recursive_digit_sum.py
@@ -0,0 +1,47 @@
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-recursive-digit-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-four
+#code:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    # Write your code here
+     initial_sum = sum(int(digit) for digit in n)
+     total_sum = initial_sum * k
+     def recursive_super_digit(x):
+         if x < 10: 
+             return x
+         return recursive_super_digit(sum(int(d) for d in str(x)))
+     return recursive_super_digit(total_sum)
+    
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+               
