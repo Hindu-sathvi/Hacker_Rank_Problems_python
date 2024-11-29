5e762f58333f6dae1bd2545c7e4b7af513bcd279
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-11-29 20:57:49 +0530

    Create Big_Sorting.py

---
+++ Big_Sorting.py
@@ -0,0 +1,83 @@
+#https://www.hackerrank.com/challenges/big-sorting/problem?isFullScreen=true
+#CODE-1[USING LAMBDA FUNCTION]
+#!/bin/python3
+
+import math
+import os
+i mport random
+import re
+import sys
+
+#
+# Complete the 'bigSorting' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts STRING_ARRAY unsorted as parameter.
+#
+
+def bigSorting(unsorted):
+    # Write your code here
+    sorted_array = sorted(unsorted, key=lambda x: (len(x), x))
+    return sorted_array
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
+
+#CODE-2
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bigSorting' function below.
+#
+# The function is expected to return a STRING_ARRAY.
+# The function accepts STRING_ARRAY unsorted as parameter.
+#
+
+def bigSorting(unsorted):
+    # Write your code here
+    def sorting_key(x):
+        return (len(x), x)
+
+    # Sort using the helper function as the key
+    return sorted(unsorted, key=sorting_key)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
+
+
