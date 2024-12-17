088e7d4da21bb96e9e785c576e4b1b724bfa61c7
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2024-12-17 20:03:05 +0530

    Create IceCareamParlor.py

---
+++ IceCareamParlor.py
@@ -0,0 +1,47 @@
+#https://www.hackerrank.com/challenges/icecream-parlor/problem?isFullScreen=true
+#Code:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    cost_map = {}
+    for i, c in enumerate(cost):
+        complement = m - c
+        if complement in cost_map:
+            return [cost_map[complement] + 1, i + 1]  # Return 1-based indices
+        cost_map[c] = i 
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
