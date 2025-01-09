b0b4aac7a2eec20ea20aec62b0d2cdb2704d8264
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2025-01-09 23:27:45 +0530

    Create Counting_Sort_2.py

---
+++ Counting_Sort_2.py
@@ -0,0 +1,44 @@
+#https://www.hackerrank.com/challenges/countingsort2/problem?isFullScreen=true
+#Code:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    max_value = max(arr)
+    count = [0] * (max_value + 1)
+    for num in arr:
+        count[num] += 1
+    sorted_arr = []
+    for i in range(len(count)):
+        sorted_arr.extend([i] * count[i])
+    
+    return sorted_arr
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
