0e32390f57729c1d28f5193a540892d79f401984
Hinduja Reddy Kora <hindujareddy426@gmail.com>
2025-01-29 23:46:18 +0530

    Create Beautiful_Triplets.py

---
+++ Beautiful_Triplets.py
@@ -0,0 +1,46 @@
+#https://www.hackerrank.com/challenges/beautiful-triplets/problem?isFullScreen=true
+#Code:
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def beautifulTriplets(d, arr):
+    # Write your code here
+    count = 0
+    values = set(arr)  # Convert list to set for O(1) lookups
+    
+    for num in arr:
+        if num + d in values and num + 2 * d in values:
+            count += 1
+
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
